489cd64d81542b02628b5a3917ee7129496e6ddb
E_Ghafour <e.ghafour2000@gmail.com>
2023-04-17 11:38:16 +0330

    adding remove_pun, and lower_case, remove_stopword and steam

--- preprocessing/data_preprocessing.py
+++ preprocessing/data_preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from nltk.corpus import stopwords
+import nltk
 import re
 
 
@@ -8,13 +9,27 @@ def preprocess(file_path,
                 fillna = 'ukw',
                 remove_punc = True,
                 lower_case = True,
-                steam = True,
                 remove_stopword = True,
+                steam = False,
 
             ):
     
     df = pd.read_csv(file_path)
     df.fillna(fillna, inplace=True)
-    df['content'] = df['title'] + ' __eoft__ ' + df['text']
-    
+    X = df['title'] + ' eoft ' + df['text']
+    y = df['label']
+
+
+    X = X.apply(lambda x: re.sub('[^a-zA-Z]', ' ', x))  if remove_punc else X
+    X = X.apply(lambda x: x.lower()) if lower_case else X
+
+    #tokenizing
+    X = X.apply(lambda x: nltk.word_tokenize(x)) 
+
+
+    X = X.apply(lambda x: [word for word in x if word not in stopwords.words('english')]) if remove_stopword else X
+
+    X = X.apply(lambda x: [])
+
+
 
