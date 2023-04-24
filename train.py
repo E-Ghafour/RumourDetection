7138b85de5968b84636e67dd5950cb98fa808ef5
E_Ghafour <e.ghafour2000@gmail.com>
2023-04-24 21:20:53 +0330

    add config.ini for putting configs

--- train.py
+++ train.py
@@ -33,17 +33,17 @@ def train(dataloader, model, loss_fn, optimizer, device):
 
 if __name__ == '__main__':
 
-    c = ConfigParser()
-    c.read('config.ini')
+    config = ConfigParser()
+    config.read('config.ini')
 
     #create/tune your own model here...
 
     #preprocess the data and save it to the file
-    fake_news_train_path = c.get('DATA', 'fake_news_train_path')
-    remove_punc = c.getboolean('PREPROCESSING', 'remove_punc')
-    lower_case = c.getboolean('PREPROCESSING', 'lower_case')
-    remove_stopwords = c.getboolean('PREPROCESSING', 'remove_stopwords')
-    steam = c.getboolean('PREPROCESSING', 'steam')
+    fake_news_train_path = config.get('DATA', 'fake_news_train_path')
+    remove_punc = config.getboolean('PREPROCESSING', 'remove_punc')
+    lower_case = config.getboolean('PREPROCESSING', 'lower_case')
+    remove_stopwords = config.getboolean('PREPROCESSING', 'remove_stopwords')
+    steam = config.getboolean('PREPROCESSING', 'steam')
 
     X, y = data_preprocessing.preprocess(fake_news_train_path,
                                         fillna='unk',
@@ -54,6 +54,38 @@ if __name__ == '__main__':
     
     data_preprocessing.save_data(X, 'x_train.data')
     data_preprocessing.save_data(y, 'y_train.data')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    model_type = config.get('MODEL_INFO', 'type')
+    input_size = config.getint('MODEL_INFO', 'input_size')
+    hidden_size = config.getint('MODEL_INFO', 'hidden_size')
+    output_size = config.getint('MODEL_INFO', 'output_size')
+    learning_rate = config.getfloat('MODEL_INFO', 'learning_rate')
+    batch_size = config.getint('MODEL_INFO', 'batch_size')
+    n_layer = config.getint('MODEL_INFO', 'n_layer')
+    bidirectional = config.getboolean('MODEL_INFO', 'bidirectional')
+    inner_dropout = config.getfloat('MODEL_INFO', 'inner_dropout')
+    dropout = config.getfloat('MODEL_INFO', 'dropout')
+    epochs = config.getint('MODEL_INFO', 'epochs')
+    pad_len = config.getint('MODEL_INFO', 'pad_len')
+    trainable_embedding = config.getboolean('MODEL_INFO', 'trainable_embedding')
+    embedding_type = config.get('MODEL_INFO', 'embedding_type')
+
+
 
     
 
